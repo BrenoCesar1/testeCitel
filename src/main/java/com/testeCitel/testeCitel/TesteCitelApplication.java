302a4180d2c9ef2b8eda760b65a2f61b64388af4
breno.santos <breno.santos@meta.com.br>
2024-08-03 16:52:22 -0300

    Commit init

---
+++ src/main/java/com/testeCitel/testeCitel/TesteCitelApplication.java
@@ -0,0 +1,13 @@
+package com.testeCitel.testeCitel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCitelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCitelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testeCitel/testeCitel/TesteCitelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testeCitel.testeCitel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCitelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
